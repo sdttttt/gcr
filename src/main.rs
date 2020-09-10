62d4c11aeeea36a14ad58fd5aaa70e4ce7be95fa
sdttttt <760159537@qq.com>
2020-09-10 15:05:15 +0800

    feat: 🎸 Add show type

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     
     let output = Command::new("git").arg("help").output().expect("Execute Failed.");
     
+    println!("Welcome to GRC");
+
     println!("Status: {}", output.status);
     let result = String::from_utf8_lossy(&output.stdout);
 
