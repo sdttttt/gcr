95f953f333db8a08066c51eab0da5e0102694504
sdttttt <760159537@qq.com>
2020-09-10 15:06:22 +0800

    feat: 🎸 Complete show_type

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use std::process::Command;
+use std::io;
 
 fn main() {
-    
-    let output = Command::new("git").arg("help").output().expect("Execute Failed.");
-    
-    println!("Welcome to GRC");
+    println!("Type:");
+    show_type();
+    let typestr = type_input();
+    println!("{}",typestr);
+}
 
-    println!("Status: {}", output.status);
-    let result = String::from_utf8_lossy(&output.stdout);
+fn show_type() {
+    println!("test:       Adding missing tests");
+    println!("feat:       A new feature");
+    println!("fix:        A bug fix");
+    println!("chore:      Build process or auxiliary tool changes");
+    println!("docs:       Documentation only changes");
+    println!("refactor:   A code change that neither fixes a bug or adds a feature");
+    println!("style:      Markup, white-space, formatting, missing semi-colons...");
+    println!("perf:       A code change that improves performance");
+    println!("ci:         CI related changes");
+}
 
-    println!("{}", result)
+fn type_input() -> String {
+    print!("Commit Message: ");
+
+    let mut result = String::new();
+    io::stdin().read_line(&mut result).expect("not string");
+    result
 }
