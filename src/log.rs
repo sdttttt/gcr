a921c60911064bf5c66858b890a6b42502ea9fce
sdttttt <760159537@qq.com>
2020-09-20 19:33:35 +0800

    feat(log.rs):Add Log function.

---
+++ src/log.rs
@@ -0,0 +1,10 @@
+use std::io;
+use std::fmt::Display;
+
+pub fn gcr_println(content: impl Display) {
+    println!("GCR: {}", content)
+}
+
+pub fn gcr_err_println(content: impl Display) {
+    println!("GCR(ERROR): {}", content)
+}
