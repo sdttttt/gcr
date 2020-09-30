e83732bdea25106dc2472b28548868ee703b8f65
sdttttt <760159537@qq.com>
2020-09-30 15:10:31 +0800

    docs: complete command help information.

--- src/metadata.rs
+++ src/metadata.rs
@@ -13,12 +13,13 @@ pub enum Mode {
 }
 
 pub const ADD_COMMAND: &str = "add";
+pub const ADD_PARAMS: &str = "add_file_name";
 pub const ADD_COMMAND_SHORT: &str = "a";
-// TODO: Fill in the `add` command help
-pub const ADD_COMMAND_HELP: &str = "...";
+pub const ADD_COMMAND_HELP: &str = "Help you add files before commit. If the parameter is `.`, Then GRC will help you add everything.";
 pub const ADD_COMMAND_NO_FILE: &str = "The add command requires parameters.";
 
 pub const PUSH_COMMAND: &str = "push";
+pub const PUSH_PARAMS: &str = "push_file_name";
 pub const PUSH_COMMAND_SHORT: &str = "p";
 // TODO: Fill in the `push` command help
 pub const PUSH_COMMAND_HELP: &str = "...";
