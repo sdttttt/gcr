26b7c974bc8749904195d9305ddd09072db5d27d
sdttttt <760159537@qq.com>
2020-10-30 15:16:14 +0800

    test(configuration): Add deserialize unit test.

--- src/configuration.rs
+++ src/configuration.rs
@@ -18,8 +18,12 @@ impl GrcConfig {
     }
 
     pub fn from(filename: &str) -> Result<Option<Self>, Error> {
-        let file_str = Self::read_config_file(GRC_CONFIG_FILE_NAME)?;
+        let file_str = Self::read_config_file(filename)?;
         Ok(Self::deserialize(file_str)?)
+    }
+
+    pub fn types(&self) -> &Option<Vec<String>> {
+        &self.typ
     }
 
     fn deserialize(file_str: String) -> Result<Option<Self>, Error> {
@@ -27,8 +31,8 @@ impl GrcConfig {
             return Ok(None);
         }
 
-        let config = toml::from_str(file_str.as_str())?;
-        Ok(config)
+        let config = toml::from_str::<GrcConfig>(file_str.as_str())?;
+        Ok(Some(config))
     }
 
     fn read_config_file(filename: &str) -> Result<String, Error> {
@@ -48,18 +52,30 @@ impl GrcConfig {
 #[cfg(test)]
 mod tests {
 
-    use std::fs;
-    use std::io::ErrorKind;
-
     use super::*;
 
     #[test]
     fn it_read_config_file() {
+        let target1 = r#"type = [ "123" ]"#;
         let file_str = GrcConfig::read_config_file("grc.test.toml").unwrap();
-        assert_eq!(file_str.as_str(), "type = \"123\"");
+        assert_eq!(file_str.as_str(), target1);
 
         let file_str2 = GrcConfig::read_config_file("nullfile").unwrap();
         assert_eq!(file_str2.len(), 0);
         assert_eq!(file_str2.as_str(), "");
+
+        let target2 = r#"type = [ "version: version is change." ]"#;
+        let config = GrcConfig::read_config_file(GRC_CONFIG_FILE_NAME).unwrap();
+        assert_eq!(config.as_str(), target2);
+    }
+    #[test]
+    fn it_deserialize() {
+        let file_str = String::from(r#"type = [ "version: version is change." ]"#);
+        let result = GrcConfig::deserialize(file_str).unwrap().unwrap();
+
+        let target = "version: version is change.";
+        let types = result.types().as_ref().unwrap();
+
+        assert_eq!(types[0], target);
     }
 }
