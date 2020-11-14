66a38ce4dc80b3582be9b00aab2cc546bae922fa
sdttttt <sdttttt@outlook.com>
2020-11-14 23:35:52 +0800

    feat(log): Add WARN print.

--- src/log.rs
+++ src/log.rs
@@ -2,15 +2,23 @@ use std::fmt::Display;
 
 use console::Style;
 
-// GCR logger.
+/// GCR logger.
 pub fn grc_println(content: impl Display) {
     println!("GRC: {}", content)
 }
 
-// GCR error logger.
+/// GCR error logger.
 pub fn grc_err_println(content: impl Display) {
     let color = Style::new().red();
-    let output = format!("GRC(ERROR): {}", content);
+    let output = format!("GRC: {}", content);
+
+    println!("{}", color.apply_to(output))
+}
+
+/// GCR error logger.
+pub fn grc_warn_println(content: impl Display) {
+    let color = Style::new().yellow();
+    let output = format!("GRC: {}", content);
 
     println!("{}", color.apply_to(output))
 }
@@ -21,12 +29,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_gcr_println() {
+    fn it_grc_println() {
         grc_println("TEST CONTENT.");
     }
 
     #[test]
-    fn test_gcr_err_println() {
+    fn it_grc_err_println() {
         grc_err_println("TEST ERROR CONTENT.");
-    }
+	}
+	
+
+	#[test]
+	fn it_grc_warn_println() {
+		grc_warn_println("TEST WARN CONTENT.");
+	}
 }
