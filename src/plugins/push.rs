f236646b824cb022037941c85bb2ad029b805456
sdttttt <sdttttt@outlook.com>
2021-02-08 17:05:36 +0800

    feat(plug): 🎉 complete push plugin.

--- src/plugins/push.rs
+++ src/plugins/push.rs
@@ -1,6 +1,7 @@
-	use git2::{Error, Direction};
-
 use crate::repo::Repository;
+use git2::{Cred, Direction, Error, PushOptions, RemoteCallbacks};
+use std::env;
+use std::path::Path;
 
 use super::CommitPlugin;
 
@@ -14,13 +15,31 @@ impl PushPlugin {
 }
 
 impl CommitPlugin for PushPlugin {
-
-	fn after(&self, repo: &Repository) ->  Result<(), Error> {
+	fn after(&self, repo: &Repository) -> Result<(), Error> {
 		println!("[+] running push ...");
 		let real_repo = repo.real_repo();
+
+		let head = real_repo.head()?;
+		let head = head.shorthand().unwrap_or_else(|| "");
+
 		let mut remote = real_repo.find_remote("origin")?;
-		remote.connect(Direction::Push)?;
-		remote.push(&[""], None)?;
+		let mut callbacks = RemoteCallbacks::new();
+		callbacks.credentials(|_, username_from_url, _| {
+			Cred::ssh_key(
+				username_from_url.unwrap(),
+				None,
+				Path::new(&format!("{}/.ssh/id_rsa", env::var("HOME").unwrap())),
+				None,
+			)
+		});
+
+		let mut push_option: PushOptions = PushOptions::new();
+		push_option.remote_callbacks(callbacks);
+
+		remote.push(
+			&[format!("refs/heads/{}:refs/heads/{}", head, head).as_str()],
+			Some(&mut push_option),
+		)?;
 		Ok(())
 	}
 }
